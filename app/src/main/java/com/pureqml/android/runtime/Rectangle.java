4c6797885b3b3f905a078c23fd1c80c6d63aa3fb
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2018-01-25 00:09:30 +0000

    split paints into FILL and STROKE, made border lazy

--- app/src/main/java/com/pureqml/android/runtime/Rectangle.java
+++ app/src/main/java/com/pureqml/android/runtime/Rectangle.java
@@ -1,11 +1,9 @@
 package com.pureqml.android.runtime;
 
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
-import android.util.Log;
 
 import com.pureqml.android.IExecutionEnvironment;
 
@@ -14,17 +12,26 @@ import static com.pureqml.android.runtime.TypeConverter.*;
 public class Rectangle extends Element {
     private final static String TAG = "rt.Rectangle";
     private Paint   _background = new Paint();
+    private Paint   _border;
     int             _radius;
 
     public Rectangle(IExecutionEnvironment env) {
         super(env);
-        _background.setStyle(Paint.Style.FILL_AND_STROKE);
+        _background.setStyle(Paint.Style.FILL);
+    }
+    Paint getBorder() {
+        if (_border == null) {
+            _border = new Paint(Paint.ANTI_ALIAS_FLAG);
+            _border.setStyle(Paint.Style.STROKE);
+        }
+        return _border;
     }
 
     protected void setStyle(String name, Object value) throws Exception {
         switch(name) {
             case "background-color":    _background.setColor(toColor((String)value)); break;
-            case "border-width":        _background.setStrokeWidth(toInteger(value)); break;
+            case "border-color":        getBorder().setColor(toColor((String)value)); break;
+            case "border-width":        getBorder().setStrokeWidth(toInteger(value)); break;
             case "border-radius":       _radius = toInteger(value); break;
             default:
                 super.setStyle(name, value); return;
@@ -43,10 +50,20 @@ public class Rectangle extends Element {
             return;
 
         Rect rect = translateRect(_rect, baseX, baseY);
-        if (_radius > 0)
+        if (_radius > 0) {
             canvas.drawRoundRect(new RectF(rect), _radius, _radius, patchAlpha(_background, opacity));
-        else
+        } else {
             canvas.drawRect(rect, patchAlpha(_background, opacity));
+        }
+
+        if (_border != null) {
+            if (_radius > 0) {
+                canvas.drawRoundRect(new RectF(rect), _radius, _radius, patchAlpha(_border, opacity));
+            } else {
+                canvas.drawRect(rect, patchAlpha(_border, opacity));
+            }
+        }
+
         super.paint(canvas, baseX, baseY, opacity);
     }
 }
