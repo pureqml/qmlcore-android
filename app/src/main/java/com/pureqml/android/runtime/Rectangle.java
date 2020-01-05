1c9c7bc4ddfa0379024849f013a6bd7825ef5ce2
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2020-01-05 23:09:10 +0000

    add Element.setupPaint

--- app/src/main/java/com/pureqml/android/runtime/Rectangle.java
+++ app/src/main/java/com/pureqml/android/runtime/Rectangle.java
@@ -2,6 +2,8 @@ package com.pureqml.android.runtime;
 
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
 
@@ -11,18 +13,26 @@ import static com.pureqml.android.runtime.TypeConverter.*;
 
 public final class Rectangle extends Element {
     private final static String TAG = "rt.Rectangle";
-    private Paint   _background = new Paint();
+    private Paint   _background;
     private Paint   _border;
     int             _radius;
 
+    private void setupPaint(Paint paint) {
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.OVERLAY));
+    }
+
     public Rectangle(IExecutionEnvironment env) {
         super(env);
+        _background = new Paint(Paint.ANTI_ALIAS_FLAG);
         _background.setStyle(Paint.Style.FILL);
+        setupPaint(_background);
     }
+
     Paint getBorder() {
         if (_border == null) {
             _border = new Paint(Paint.ANTI_ALIAS_FLAG);
             _border.setStyle(Paint.Style.STROKE);
+            setupPaint(_border);
         }
         return _border;
     }
