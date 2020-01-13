6e61ef32f172f50dc55d11af7b3576023449c6cc
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2020-01-13 22:07:01 +0000

    output trim option

--- app/src/main/java/com/pureqml/android/MainActivity.java
+++ app/src/main/java/com/pureqml/android/MainActivity.java
@@ -200,7 +200,32 @@ public final class MainActivity
 
     @Override
     public void onTrimMemory(int level) {
-        Log.i(TAG, "trim memory, level: " + level);
+        switch(level)
+        {
+            case TRIM_MEMORY_RUNNING_MODERATE:
+                Log.i(TAG, "trim memory, TRIM_MEMORY_RUNNING_MODERATE");
+                break;
+            case TRIM_MEMORY_RUNNING_LOW:
+                Log.i(TAG, "trim memory, TRIM_MEMORY_RUNNING_LOW");
+                break;
+            case TRIM_MEMORY_RUNNING_CRITICAL:
+                Log.i(TAG, "trim memory, TRIM_MEMORY_RUNNING_CRITICAL");
+                break;
+            case TRIM_MEMORY_UI_HIDDEN:
+                Log.i(TAG, "trim memory, TRIM_MEMORY_UI_HIDDEN");
+                break;
+            case TRIM_MEMORY_BACKGROUND:
+                Log.i(TAG, "trim memory, TRIM_MEMORY_BACKGROUND");
+                break;
+            case TRIM_MEMORY_MODERATE:
+                Log.i(TAG, "trim memory, TRIM_MEMORY_MODERATE");
+                break;
+            case TRIM_MEMORY_COMPLETE:
+                Log.i(TAG, "trim memory, TRIM_MEMORY_COMPLETE");
+                break;
+            default:
+                Log.i(TAG, "trim memory, level: " + level);
+        }
         super.onTrimMemory(level);
     }
 }
