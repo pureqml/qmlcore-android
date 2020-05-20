c170fb574e001e9a6f9ca83736337d7e63ace3c0
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2020-05-20 21:59:22 +0100

    implement linear gradients

--- app/src/main/java/com/pureqml/android/runtime/Rectangle.java
+++ app/src/main/java/com/pureqml/android/runtime/Rectangle.java
@@ -2,14 +2,19 @@ package com.pureqml.android.runtime;
 
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.Shader;
 import android.util.Log;
 
+import com.eclipsesource.v8.V8Array;
+import com.eclipsesource.v8.V8Object;
 import com.pureqml.android.IExecutionEnvironment;
+import com.pureqml.android.TypeConverter;
 
 import static com.pureqml.android.TypeConverter.toColor;
 import static com.pureqml.android.TypeConverter.toInteger;
@@ -20,6 +25,9 @@ public final class Rectangle extends Element {
     private Paint   _border;
     int             _radius = 0;
     int             _color = 0;
+    String          _gradientOrientation = null;
+    int             _gradientColors[] = null;
+    float           _gradientPositions[] = null;
 
     private void setupPaint(Paint paint) {
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
@@ -41,12 +49,64 @@ public final class Rectangle extends Element {
         return _border;
     }
 
+    private void parseBackgroundDescription(Object value) {
+        if (!(value instanceof V8Object)) {
+            Log.d(TAG, "invalid background descriptor, non-object: " + value);
+            return;
+        }
+        V8Object descriptor = (V8Object)value;
+        _gradientOrientation = descriptor.getString("orientation");
+        Log.v(TAG, "gradient orientation: " + _gradientOrientation);
+        V8Array stops = descriptor.getArray("stops");
+
+        _gradientColors = new int[stops.length()];
+        _gradientPositions = new float[stops.length()];
+
+        for(int i = 0; i < stops.length(); ++i) {
+            V8Object stop = stops.getObject(i);
+            _gradientColors[i] = TypeConverter.toColor(stop.get("color"));
+            _gradientPositions[i] = TypeConverter.toFloat(stop.get("position"));
+        }
+    }
+
+    Rect getGradientRect(Rect rect) {
+        Rect r = new Rect(rect);
+        switch(_gradientOrientation)
+        {
+            case "to bottom":
+                r.right = r.left;
+                break;
+            case "to top right":
+                r.bottom = r.top;
+                break;
+            case "to bottom right":
+                break;
+            case "to left":
+                r.bottom = r.top;
+                int l = r.left;
+                r.left = r.right;
+                r.right = l;
+                break;
+            default:
+                Log.d(TAG, "unsupported gradient orientation");
+        }
+        return r;
+    }
+
     @Override
     protected void setStyle(String name, Object value) {
         switch(name) {
-            case "background-color":    _color = toColor(value); _background.setAlpha(Color.alpha(_color)); _background.setColor(_color); break;
+            case "background":          parseBackgroundDescription(value); break;
             case "border-color":        getBorder().setColor(toColor(value)); break;
             case "border-width":        getBorder().setStrokeWidth(toInteger(value)); break;
+            case "background-color":
+                _color = toColor(value);
+                _background.setAlpha(Color.alpha(_color));
+                _background.setColor(_color);
+                _gradientColors = null;
+                _gradientPositions = null;
+                _gradientOrientation = null;
+                break;
             case "border-radius":
                 try
                 { _radius = toInteger(value); }
@@ -68,7 +128,19 @@ public final class Rectangle extends Element {
         float opacity = state.opacity;
         Rect rect = getDstRect(state);
 
-        if (_background.getColor() != 0) {
+        if (_background.getColor() != 0 || _gradientOrientation != null) {
+            if (_gradientOrientation != null) {
+                Rect gradientRect = getGradientRect(rect);
+                LinearGradient lg = new LinearGradient(
+                        gradientRect.left, gradientRect.top,
+                        gradientRect.right, gradientRect.bottom,
+                        _gradientColors, _gradientPositions, Shader.TileMode.CLAMP
+                );
+                _background.setShader(lg);
+            }
+            else
+                _background.setShader(null);
+
             Paint paint = patchAlpha(_background, Color.alpha(_color), opacity);
             if (paint != null) {
                 if (_radius > 0) {
