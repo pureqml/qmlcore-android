a0c8200fea4abef99f372571bb05ff0629359c9e
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2020-12-18 18:51:04 +0000

    implement async local storage

--- app/src/main/java/com/pureqml/android/runtime/LocalStorage.java
+++ app/src/main/java/com/pureqml/android/runtime/LocalStorage.java
@@ -12,20 +12,45 @@ import com.pureqml.android.IExecutionEnvironment;
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public final class LocalStorage extends BaseObject {
     private static final String TAG = "localstorage";
     private final int BufferSize = 128 * 1024;
+    private final ExecutorService _threadPool;
+    private final ExecutorService _scriptThread;
 
     public LocalStorage(IExecutionEnvironment env) {
         super(env);
         Log.i(TAG, "local storage created");
+        _threadPool = env.getThreadPool();
+        _scriptThread = _env.getExecutor();
     }
 
-    public void get(String name, V8Function callback, V8Function error, V8Object origin) {
-        Log.i(TAG, "getting value " + name);
-        V8Array args = new V8Array(_env.getRuntime());
-        Object ret = null;
+    private void notify(V8Function callback, V8Object origin, String data) {
+        _scriptThread.submit(new Runnable() {
+            @Override
+            public void run() {
+                V8Array args = new V8Array(_env.getRuntime());
+                Object ret = null;
+                try {
+                    args.push(data);
+                    ret = callback.call(origin, args);
+                } catch (Exception ex) {
+                    Log.w(TAG, "completion callback failed", ex);
+                } finally {
+                    if (ret instanceof Releasable)
+                        ((Releasable)ret).release();
+                    args.close();
+                }
+            }
+        });
+    }
+
+    private void getAsync(String name, V8Function callback, V8Function error, V8Object origin) {
+        Log.i(TAG, "getting value async " + name);
         try {
             FileInputStream file = _env.getContext().openFileInput(name + ".storage");
             Log.d(TAG, "opened file " + name + " for reading...");
@@ -35,54 +60,69 @@ public final class LocalStorage extends BaseObject {
             while ((r = file.read(buffer)) != -1) {
                 data.write(buffer, 0, r);
             }
-            args.push(data.toString("UTF-8"));
-            ret = callback.call(origin, args);
+            file.close();
+
+            String value = data.toString("UTF-8");
+            data.close();
+            notify(callback, origin, value);
         } catch (Exception ex) {
-            Log.w(TAG, "can't open file " + name + " for reading");
-            args.push(ex.toString());
-            ret = error.call(origin, args); //indicate error
-        } finally {
-            if (ret instanceof Releasable)
-                ((Releasable)ret).release();
-            args.close();
+            Log.v(TAG, "can't open file " + name + " for reading", ex);
+            notify(error, origin, ex.toString());
+        }
+    }
+
+    public void get(String name, V8Function callback, V8Function error, V8Object origin) {
+        Log.i(TAG, "getting value " + name);
+        _threadPool.submit(new Runnable() {
+            @Override
+            public void run() {
+                getAsync(name, callback, error, origin);
+            }
+        });
+    }
+
+    private void setAsync(String name, String value, V8Function error, V8Object origin) {
+        Log.i(TAG, "setting value async " + name);
+        try {
+            FileOutputStream file = _env.getContext().openFileOutput(name + ".storage", Context.MODE_PRIVATE);
+            Log.i(TAG, "opened file for writing...");
+            file.write(value.getBytes("UTF-8"));
+            file.close();
+        } catch (Exception ex) {
+            Log.e(TAG, "can't open file " + name + " for writing", ex);
+            notify(error, origin, ex.toString());
         }
     }
 
     public void set(String name, String value, V8Function error, V8Object origin) {
         Log.i(TAG, "setting value " + name);
-        V8Array args = new V8Array(_env.getRuntime());
-        Object ret = null;
+        _threadPool.submit(new Runnable() {
+            @Override
+            public void run() {
+                setAsync(name, value, error, origin);
+            }
+        });
+    }
+
+    public void eraseAsync(String name, V8Function error, V8Object origin) {
+        Log.i(TAG, "erasing value async " + name);
         try {
-            FileOutputStream file = _env.getContext().openFileOutput(name + ".storage", Context.MODE_PRIVATE);
-            Log.i(TAG, "opened file for writing...");
-            file.write(value.getBytes("UTF-8"));
+            _env.getContext().deleteFile(name + ".storage");
+            Log.i(TAG, "file deleted...");
         } catch (Exception ex) {
-            Log.e(TAG, "can't open file for writing");
-            args.push(ex.toString());
-            ret = error.call(origin, args); //indicate error
-        } finally {
-            if (ret instanceof Releasable)
-                ((Releasable)ret).release();
-            args.close();
+            Log.e(TAG, "can't delete file", ex);
+            notify(error, origin, ex.toString());
         }
     }
 
     public void erase(String name, V8Function error, V8Object origin) {
         Log.i(TAG, "erasing value " + name);
-        V8Array args = new V8Array(_env.getRuntime());
-        Object ret = null;
-        try {
-            _env.getContext().deleteFile(name + ".storage");
-            Log.i(TAG, "file deleted...");
-        } catch (Exception ex) {
-            Log.e(TAG, "can't delete file");
-            args.push(ex.toString());
-            ret = error.call(origin, args); //indicate error
-        } finally {
-            if (ret instanceof Releasable)
-                ((Releasable)ret).release();
-            args.close();
-        }
+        _threadPool.submit(new Runnable() {
+            @Override
+            public void run() {
+                eraseAsync(name, error, origin);
+            }
+        });
     }
 
 }
